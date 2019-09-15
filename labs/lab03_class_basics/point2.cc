d217a8fd91dac0c91a3d444df98b4d132e4d9c86
Mootii <mamox017@umn.edu>
2019-09-14 20:12:52 -0500

    lol

--- labs/lab03_class_basics/point2.cc
+++ labs/lab03_class_basics/point2.cc
@@ -10,13 +10,17 @@ Point2::Point2(int x, int y){
 }
 
 int Point2::Quadrant(){
-	if (xcoord > 0 && ycoord < 0){
+	//quadrants
+	if (xcoord == 0 && ycoord == 0){
+		return 0;
+	}
+	else if (xcoord >= 0 && ycoord < 0){
 		return 4;
 	}
-	else if (xcoord < 0 && ycoord < 0){
+	else if (xcoord < 0 && ycoord <= 0){
 		return 3;
 	}
-	else if (xcoord < 0 && ycoord > 0){
+	else if ((xcoord <= 0 && ycoord > 0)){
 		return 2;
 	}
 	else {
@@ -31,5 +35,5 @@ void Point2::Print(){
 float Point2::DistanceBetween(Point2 p2){
 	int a = p2.get_x() - this->get_x();
 	int b = p2.get_y() - this->get_y();
-	return sqrt(pow(a,2)+pow(b,2));
+	return sqrt(pow(a,2.0)+pow(b,2.0));
 }
