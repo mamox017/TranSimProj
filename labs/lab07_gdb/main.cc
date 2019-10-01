f811c917648035f61d82a97976760b83228e96b1
Mootii <mamox017@umn.edu>
2019-09-30 20:50:11 -0500

    lol

--- labs/lab07_gdb/main.cc
+++ labs/lab07_gdb/main.cc
@@ -13,7 +13,8 @@ using namespace std;
 
 void doInterestingThing(int yr) {
   // variables used in this method
-  int* array_len;
+  int x = 4;
+  int* array_len = &x;
   Date d1;
   Date d2;
   Date d3;
@@ -21,7 +22,7 @@ void doInterestingThing(int yr) {
 
   cout << " ... A set of Dates ... " << endl;
 
-  array_len = NULL;
+  //array_len = NULL;
   d2 = Date(yr, 4, 19);
   d3 = d2.copy();
   *array_len = 4;
@@ -41,20 +42,22 @@ void doInterestingThing(int yr) {
   }
 
   // now show the dates accumulated in reverse order
-  for (int i = 0; i < *array_len; --i) {
+  for (int i = *array_len-1; i >=0; i--) {
     cout << "date is: " << birthday_list[i]->show() << endl;
   }
 
   // now set the dates to January 2
-  for (int i = 0; i <= *array_len; i++) {
+  for (int i = 0; i < *array_len; i++) {
     *birthday_list[i] = Date(1990+i, 1, 2);
   }
   
   cout << "the first date is: " << birthday_list[0]->show() << endl;
+  delete[] birthday_list;
 }
 
 int main() {
   doInterestingThing(1998);
   doInterestingThing(1999);
   cout << endl << "... Goodbye ..." << endl;
+  return 0;
 }
