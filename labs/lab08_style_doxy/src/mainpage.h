8c86359393c69f0ed6c43c8ef6d4f85053210b8d
Mootii <mamox017@umn.edu>
2019-10-07 20:48:31 -0500

    commit

--- labs/lab08_style_doxy/src/mainpage.h
+++ labs/lab08_style_doxy/src/mainpage.h
@@ -1,7 +1,12 @@
-/*! \mainpage My Personal Index Page
+/*! Copyright 2019 Mootii Mamo
+   \mainpage My Personal Index Page
  * \title DOXY PAGE
  * \section intro_sec Introduction
  *
  * This is the introduction.
  * Hello I am Mootii Mamo, welcome to my page.
  */
+#ifndef SRC_MAINPAGE_H_
+#define SRC_MAINPAGE_H_
+
+#endif  // SRC_MAINPAGE_H_
