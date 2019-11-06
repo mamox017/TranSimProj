efec1a2eb46c9139ec111a47dceb996694fa77ae
Mootii <mamox017@umn.edu>
2019-11-06 12:41:54 -0600

    start of mainpage

--- project/src/mainpage.h
+++ project/src/mainpage.h
@@ -1,6 +1,6 @@
-/*! \mainpage My Personal Index Page
+/*! \mainpage Mootii Mamo, CSCI 3081W, mamox017@umn.edu
  *
- * \section intro_sec Introduction
+ * \section intro_sec Project TransitSim: a Proof-of-Concept Transit System Simulator: Iteration 1
  *
  * This is the introduction.
  *
