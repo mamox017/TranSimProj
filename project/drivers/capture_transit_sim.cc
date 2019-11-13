f16627677bcc437df03052ef0f149cf17f419ebe
Mootii <mamox017@umn.edu>
2019-11-13 14:37:18 -0600

    fixed makefile

---
+++ project/drivers/capture_transit_sim.cc
@@ -0,0 +1,42 @@
+#include <ctime>
+#include <iostream>
+#include <random>
+#include <fstream>
+#include "local_simulator.h"
+
+int main(int argc, char * argv[]) {
+    
+  int rounds = 50; //Number of rounds of generation to simulate in test
+  std::ofstream myFilePtr;
+  if (argc > 1) {
+  	myFilePtr.open(argv[1]);
+	srand((long)time(NULL)); //Seed the random number generator...
+	  
+	LocalSimulator my_sim;
+	  
+	if (myFilePtr.is_open()) {
+		  myFilePtr << "/*************************" << std::endl << std::endl;
+		  myFilePtr << "         STARTING" << std::endl;
+		  myFilePtr << "        SIMULATION" << std::endl;
+		  myFilePtr << "*************************/" << std::endl;
+		  
+		  my_sim.Start(myFilePtr);
+		  
+		  myFilePtr << "/*************************" << std::endl << std::endl;
+		  myFilePtr << "           BEGIN" << std::endl;
+		  myFilePtr << "          UPDATING" << std::endl;
+		  myFilePtr << "*************************/" << std::endl;
+		  
+		  for (int i = 0; i < rounds; i++) {
+			  my_sim.Update(myFilePtr);
+		  }
+		  
+		  myFilePtr << "/*************************" << std::endl << std::endl;
+		  myFilePtr << "        SIMULATION" << std::endl;
+		  myFilePtr << "         COMPLETE" << std::endl;
+		  myFilePtr << "*************************/" << std::endl;
+	  }
+  }
+
+  return 0;
+}
