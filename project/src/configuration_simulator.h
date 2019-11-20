d25b5a183c92cd631e0ed3f088f6f3e2bfcc5f46
Mootii <mamox017@umn.edu>
2019-11-19 20:22:34 -0600

    guess attempt

---
+++ project/src/configuration_simulator.h
@@ -0,0 +1,37 @@
+// Copyright
+
+#ifndef SRC_CONFIGURATION_SIMULATOR_H_
+#define SRC_CONFIGURATION_SIMULATOR_H_
+
+#include <vector>
+#include <list>
+
+#include "src/config_manager.h"
+
+class Route;
+class Bus;
+class Stop;
+
+class ConfigurationSimulator {
+ public:
+    ConfigurationSimulator(ConfigManager*);
+    ~ConfigurationSimulator();
+
+    void Start(const std::vector<int>&, const int&);
+    void Update();
+
+ private:
+    ConfigManager* configManager_;
+
+    std::vector<int> busStartTimings_;
+    std::vector<int> timeSinceLastBus_;
+    int numTimeSteps_;
+    int simulationTimeElapsed_;
+
+    std::vector<Route *> prototypeRoutes_;
+    std::vector<Bus *> busses_;
+
+    int busId = 1000;
+};
+
+#endif  // SRC_CONFIGURATION_SIMULATOR_H_
